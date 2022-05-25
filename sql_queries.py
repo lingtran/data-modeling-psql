85e445140338836f81d6a46ac9894581e5d72a08
Ling T <lxiutran@gmail.com>
2022-05-25 15:36:03 -0400

    Add on conflict handler for artists tables insertion statement

--- sql_queries.py
+++ sql_queries.py
@@ -88,6 +88,7 @@ song_table_insert = ("""
 artist_table_insert = ("""
     INSERT INTO artists (artist_id, name, location, latitude, longitude)
     VALUES (%s, %s, %s, %s, %s)
+    ON CONFLICT (artist_id) DO NOTHING
 """)
 
 time_table_insert = ("""
